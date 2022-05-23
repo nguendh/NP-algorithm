4b7901d5218d84162f565f9c1aea3ec98101773c
Nguyen Dai Hiep <73826903+nguendh@users.noreply.github.com>
2022-05-23 06:04:40 +0300

    Update test.py

--- test.py
+++ test.py
@@ -10,7 +10,7 @@ from main import Graph
 class Test(unittest.TestCase):
     def test_case1(self):
         size = random.randint(1, 1000)
-        random_list = [random.randint(0, 1) for i in range(size)]
+        random_list = [[random.randint(0, 1) for i in range(size)] for j in range(size)]
         graph = Graph(size)
         graph.set_matrix(random_list)
         brute_force = BruteForceSolution(graph)
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         self.assertEqual(brute_force.find_hamilton_cycle(), bellman.find_hamilton_cycle(), inc.find_hamilton_cycle())
 
     def test_case2(self):
-        random_list = [1 for i in range(1000)]
+        random_list = [[1 for i in range(1000)] for j in range(1000)]
         graph = Graph(1000)
         graph.set_matrix(random_list)
         brute_force = BruteForceSolution(graph)
@@ -30,7 +30,7 @@ class Test(unittest.TestCase):
         self.assertEqual(inc.find_hamilton_cycle(), True)
 
     def test_case3(self):
-        random_list = [0 for i in range(1000)]
+        random_list = [[0 for i in range(1000)] for j in range(1000)]
         graph = Graph(1000)
         graph.set_matrix(random_list)
         brute_force = BruteForceSolution(graph)
@@ -41,7 +41,10 @@ class Test(unittest.TestCase):
         self.assertEqual(inc.find_hamilton_cycle(), True)
 
     def test_case4(self):
-        random_list = [1, 5, 6, 3, 3]
+        random_list = [[0, 1, 0, 0, 1, 0], [1, 0, 1, 0, 1, 0],
+                       [0, 1, 0, 1, 0, 0], [0, 0, 1, 0, 1, 1],
+                       [1, 1, 0, 1, 0, 0], [0, 0, 0, 1, 0, 0],
+                       ]
         size = len(random_list)
         graph = Graph(size)
         graph.set_matrix(random_list)
