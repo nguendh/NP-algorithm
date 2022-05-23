8ca781295dbac00d30724669e313cabd363a0f72
Nguyen Dai Hiep <73826903+nguendh@users.noreply.github.com>
2022-05-23 14:01:16 +0300

    Update graph.py

--- graph.py
+++ graph.py
@@ -4,7 +4,7 @@ from benchmark import bellman_time
 from benchmark import inc_time
 
 
-size = [1000*(i + 1) for i in range(4)]
+size = [1000*(i + 1) for i in range(7)]
 graph.plot(size, brute_force_time, label="Brute Force Solution")
 graph.plot(size, bellman_time, label="Bellman Held Karp Solution")
 graph.plot(size, inc_time, label="Inclusion Exclusion Solution")
