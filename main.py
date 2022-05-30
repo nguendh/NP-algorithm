ceb6b0fb976dfbb7e7040a1cd4f47aa4c01e9826
Nguyen Dai Hiep <73826903+nguendh@users.noreply.github.com>
2022-05-30 17:52:24 +0300

    Update main.py
    add GUI

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import itertools
+import tkinter
+
 import numpy as np
 import time
+
+from tkinter import Tk, Label, StringVar, Button, Entry, BOTTOM, RIGHT
 
 
 def count_set_bits(n):
@@ -39,7 +43,7 @@ class BruteForceSolution:
                     break
             if hamilton_cycle:
                 break
-        print(hamilton_cycle)
+        return hamilton_cycle
 
 
 class BellmanHeldKarp:
@@ -64,7 +68,7 @@ class BellmanHeldKarp:
         for i in range(1, matrix.node_number - 1):
             result |= (path[pow(2, int(matrix.node_number - 1)) - 1][i - 1] and (
                     matrix.matrix[i][0] or matrix.matrix[0][i]))
-        print(bool(result))
+        return bool(result)
 
 
 class InclExcl:
@@ -92,6 +96,29 @@ class InclExcl:
         if np.linalg.det(matrix_new) == 0:
             hamilton_cycle = False
         print(hamilton_cycle)
+
+
+class Dirac:
+    def __init__(self, graph):
+        self.graph = graph
+
+    def findDegree(self, ver):
+        degree = 0
+        for i in range(self.graph.node_number):
+            if (self.graph.matrix[ver][i] == 1 or self.graph.matrix[ver][i] == 1):
+                degree += 1
+        return degree
+
+    def find_hamilton_cycle(self):
+        matrix = self.graph
+        hamilton_cycle = True
+        for i in range(matrix.node_number):
+            if self.findDegree(i) <= (matrix.node_number / 2):
+                hamilton_cycle = False
+                break
+        print(hamilton_cycle)
+        if (hamilton_cycle == False):
+            return BellmanHeldKarp(self.graph).find_hamilton_cycle()
 
 
 class TimeCalculating:
@@ -124,15 +151,83 @@ class TimeCalculating:
         print("Time for Inclusion Exclusion:", third_time)
 
 
+window = Tk()
+window.title("Hamilton")
+window.geometry("650x500+120+120")
+window.configure(bg='bisque2')
+window.resizable(False, False)
+NumberOfNode = Label(window, text="Enter number of nodes: ", font=('arial', 10, 'bold'), bg="bisque2")
+NumberOfNode.pack()
+T = tkinter.Text(window, height=1, width=5)
+T.pack()
+b1 = Button(window, text="Submit", command=lambda: mtx(int(T.get("1.0", "end-1c"))))
+b1.pack()
+
+text_var = []
+entries = []
+matrix = []
+
+
+def get_mat():
+    for i in range(int(T.get("1.0", "end-1c"))):
+        matrix.append([])
+        for j in range(int(T.get("1.0", "end-1c"))):
+            matrix[i].append(int(text_var[i][j].get()))
+    a = Graph(int(T.get("1.0", "end-1c")))
+    a.set_matrix(matrix)
+    b = BellmanHeldKarp(a)
+    b.find_hamilton_cycle()
+    c = BruteForceSolution(a)
+    c.find_hamilton_cycle()
+    d = Dirac(a)
+    d.find_hamilton_cycle()
+    Label(window, text="Brute Force Solution: " + str(c.find_hamilton_cycle()), font=('arial', 10, 'bold'),
+          bg="bisque2").pack()
+    Label(window, text="Bellman Held Karp Solution: " + str(b.find_hamilton_cycle()), font=('arial', 10, 'bold'),
+          bg="bisque2").pack()
+    Label(window, text="Dirac-Bellman Solution: " + str(d.find_hamilton_cycle()), font=('arial', 10, 'bold'),
+          bg="bisque2").pack()
+
+
+Label(window, text="Enter matrix: ", font=('arial', 10, 'bold'),
+      bg="bisque2").place(x=20, y=20)
+
+
+def mtx(number_of_node):
+    x2 = 0
+    y2 = 0
+    rows, cols = (number_of_node, number_of_node)
+    for i in range(rows):
+        text_var.append([])
+        entries.append([])
+        for j in range(cols):
+            text_var[i].append(StringVar())
+            entries[i].append(Entry(window, textvariable=text_var[i][j], width=3))
+            entries[i][j].place(x=60 + x2, y=80 + y2)
+            x2 += 30
+        y2 += 30
+        x2 = 0
+    button = Button(window, text="Submit", bg='bisque3', width=15, command=lambda: get_mat())
+    button.pack(side=BOTTOM)
+
+
+window.mainloop()
+
+
 ### Lets try with an example to check solution
 
-a = Graph(6)
-a.set_matrix([[0, 1, 0, 0, 1, 0],
-              [1, 0, 1, 0, 1, 0],
-              [0, 1, 0, 1, 0, 0],
-              [0, 0, 1, 0, 1, 1],
-              [1, 1, 0, 1, 0, 0],
-              [0, 0, 0, 1, 0, 0],
-              ])
-e = TimeCalculating(a)
-e.time_calculating()
+
+# a.set_matrix([[0, 1, 0, 0, 1, 0],
+#               [1, 0, 1, 0, 1, 0],
+#               [0, 1, 0, 1, 0, 0],
+#               [0, 0, 1, 0, 1, 1],
+#               [1, 1, 0, 1, 0, 1],
+#               [1, 0, 0, 0, 0, 0],
+#               ])
+# e = TimeCalculating(a)
+# e.time_calculating()
+# a.set_matrix(matrix)
+# b = Dirac(a)
+# b.find_hamilton_cycle()
+# c = BruteForceSolution(a)
+# c.find_hamilton_cycle()
